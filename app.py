49a97b8c00336a594cabf394aa8646097bcf7ddd
CustomGPTer <jacksonja21@gmail.com>
2025-06-07 11:41:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,3 +79,6 @@ Use UK construction terminology only.
 def download():
     file = request.args.get("file")
     return send_file(os.path.join("/mnt/data", file), as_attachment=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT not set
+    app.run(host="0.0.0.0", port=port)
